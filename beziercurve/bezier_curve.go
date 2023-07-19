a10b4ead30f0fb260689656b31404f0ae8e1d386
emoob <379946303@qq.com>
2023-07-19 18:17:05 +0800

    add

--- beziercurve/bezier_curve.go
+++ beziercurve/bezier_curve.go
@@ -5,10 +5,10 @@ type Point struct {
 }
 
 /*
-  - start := Point{X: 0, Y: 0}        起始位置
-    control := Point{X: 128, Y: 54}   最大较大弯曲程度
-    end := Point{X: 256, Y: 108}      结束位置
-    numPoints := 100                  点的数量
+  - start := Point{X: 0, Y: 0}         起始位置
+    control := Point{X: 128, Y: 54}    最大较大弯曲程度
+    end := Point{X: 256, Y: 108}       结束位置
+    numPoints := 100                   点的数量
     points := QuadraticBezierCurve(start, control, end, numPoints)
 */
 func QuadraticBezierCurve(start, control, end Point, numPoints int) []Point {
