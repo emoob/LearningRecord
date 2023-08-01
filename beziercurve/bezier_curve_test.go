05c2c7b13eea01ae3a009b855d6aaba3e0cb373f
emoob <379946303@qq.com>
2023-08-01 17:25:07 +0800

    add

--- beziercurve/bezier_curve_test.go
+++ beziercurve/bezier_curve_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestQuadraticBezierCurve(t *testing.T) {
-	start := Point{X: 0, Y: 0}      //     起始位置
+	start := Point{X: 0, Y: 0}      //  起始位置
 	control := Point{X: 128, Y: 54} //  最大较大弯曲程度
-	end := Point{X: 256, Y: 108}    //   结束位置
-	numPoints := 100                //   点的数量
+	end := Point{X: 256, Y: 108}    //  结束位置
+	numPoints := 100                //  点的数量
 	points := QuadraticBezierCurve(start, control, end, numPoints)
 	fmt.Printf("坐标系:%v \n", points)
 }
