09733d03a4dd3113b296c0681588ef5d9ff81671
emoob <379946303@qq.com>
2023-09-02 15:44:43 +0800

    add

--- leetcode/twoSum.go
+++ leetcode/twoSum.go
@@ -3,11 +3,14 @@ package leetcode
 func twoSum(nums []int, target int) []int {
 	prevNums := map[int]int{}
 	for i, num := range nums {
+		// 获取差值
 		targetNum := target - num
 		targetNumIndex, ok := prevNums[targetNum]
+		// 看这个字典是否有差值
 		if ok {
 			return []int{targetNumIndex, i}
 		}
+		// 不存在则存入
 		prevNums[num] = i
 	}
 	return []int{}
